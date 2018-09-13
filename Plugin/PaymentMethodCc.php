18c500fd2e04f319f90a64a36cc69be49aeb9395
Anonymous Dev <foo@bar.com>
2018-09-13 19:14:40 -0400

    Skip if PO is null

--- Plugin/PaymentMethodCc.php
+++ Plugin/PaymentMethodCc.php
@@ -12,12 +12,16 @@ class PaymentMethodCc
         CreditCardMethod $result,
         DataObject $data
     ) {
+        $purchaseOrder = $data->getPoNumber();
+        if ($purchaseOrder === null) {
+            return $result;
+        }
         /** @var \Magento\Quote\Model\Quote\Payment $info */
         $info = $subject->getInfoInstance();
         // save purchase order number to object
         $info->addData(
             [
-                'po_number' => $data->getPoNumber(),
+                'po_number' => $purchaseOrder,
             ]
         );
         return $result;
