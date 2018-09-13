6f6fdc230536c511c31481b7ddeffd6bc3c39b0a
Anonymous Dev <foo@bar.com>
2018-09-13 19:12:49 -0400

    Create plugin to save purchase order number

--- Plugin/PaymentMethodCc.php
+++ Plugin/PaymentMethodCc.php
@@ -1,8 +1,25 @@
 <?php
+
 namespace SomethingDigital\PurchaseOrderNumber\Plugin;
+
+use Magento\Payment\Model\Method\Cc as CreditCardMethod;
+use Magento\Framework\DataObject;
+
 class PaymentMethodCc
 {
-    //function beforeMETHOD($subject, $arg1, $arg2){}
-    //function aroundMETHOD($subject, $proceed, $arg1, $arg2){return $proceed($arg1, $arg2);}
-    //function afterMETHOD($subject, $result){return $result;}
+    public function afterAssignData(
+        CreditCardMethod $subject, 
+        CreditCardMethod $result,
+        DataObject $data
+    ) {
+        /** @var \Magento\Quote\Model\Quote\Payment $info */
+        $info = $subject->getInfoInstance();
+        // save purchase order number to object
+        $info->addData(
+            [
+                'po_number' => $data->getPoNumber(),
+            ]
+        );
+        return $result;
+    }
 }
