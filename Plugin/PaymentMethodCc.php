f0b8322671b25326792d1a78632986d4911726ef
Anonymous Dev <foo@bar.com>
2018-09-13 18:51:03 -0400

    Generate plugin

---
+++ Plugin/PaymentMethodCc.php
@@ -0,0 +1,8 @@
+<?php
+namespace SomethingDigital\PurchaseOrderNumber\Plugin;
+class PaymentMethodCc
+{
+    //function beforeMETHOD($subject, $arg1, $arg2){}
+    //function aroundMETHOD($subject, $proceed, $arg1, $arg2){return $proceed($arg1, $arg2);}
+    //function afterMETHOD($subject, $result){return $result;}
+}
